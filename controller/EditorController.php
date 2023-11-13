099d0ef2f9c1fe9ccc8a57da613a90c9c056826e
lautarocaputo <lautaro0611@gmail.com>
2023-11-13 02:54:44 -0300

    ranking y reportar pregunta con ajx

--- controller/EditorController.php
+++ controller/EditorController.php
@@ -5,7 +5,6 @@ class EditorController
 
     private $renderer;
     private $editorModel;
-    private $editorModel;
 
     public function __construct($editorModel, $renderer)
     {
@@ -48,22 +47,6 @@ class EditorController
         header("Location: /editor/cargar");
     }
 
-    public function aprobarODenegarPregunta(){
-        if(isset($_POST['pregunta_id'])){
-            if($_POST['action'] === 'aprobar'){
-                foreach ($_POST['pregunta_id'] as $pregunta_id){
-                    $this->editorModel->aprobarPregunta($pregunta_id);
-                }
-            }elseif ($_POST['action'] === 'denegar'){
-                foreach ($_POST['pregunta_id'] as $pregunta_id){
-                    $this->editorModel->denegarPregunta($pregunta_id);
-                }
-            }
-        }
-        $data["suggestedQuestion"] = $this->editorModel->getSuggestedQuestion();
-        $data["reportedQuestions"] = $this->editorModel->getReportedQuestionWithQuestion();
-        $this->renderer->render('editor', $data);
-    }
 
     public function aprobarODenegarPregunta(){
         if(isset($_POST['pregunta_id'])){
