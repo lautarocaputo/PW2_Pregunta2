381969b4e4d92269f48b81cc8a2ad3f9c7eb015d
LautaroC <lautaro0611@gmail.com>
2023-11-13 10:37:34 -0300

    fixes en las querys de editorModel

--- model/EditorModel.php
+++ model/EditorModel.php
@@ -27,9 +27,7 @@ class EditorModel
     }
 
     public function getRespuestasPorIdPregunta($preguntaID){
-        $query = "SELECT r.* FROM respuestas r 
-        INNER JOIN preguntas p ON r.Tematica_ID = p.Tematica_ID
-        WHERE p.Pregunta_ID = $preguntaID ORDER BY r.Correcta desc";
+        $query = "SELECT r.* FROM respuesta r WHERE r.Pregunta_ID = $preguntaID ORDER BY r.Correcta desc";
 
         return $this->database->query($query);
     }
@@ -50,23 +48,20 @@ class EditorModel
 
         $query = "UPDATE `preguntas` SET Pregunta_texto = '$pregunta' WHERE Pregunta_ID = $idPregunta";
         $this->database->update($query);
-       
-        $tematicaId = $this->getTematicaPorIdPregunta($idPregunta)[0][0];
 
-        $query = "DELETE FROM `respuestas` WHERE `Tematica_ID` = '$tematicaId'";
+        $query = "DELETE FROM `respuesta` WHERE `Pregunta_ID` = '$idPregunta'";
         $this->database->update($query);
        
-        $query = "INSERT INTO `respuestas` (`Respuesta_texto`, `Correcta`, `Tematica_ID`) VALUES ('$respuesta1', '1', '$tematicaId')";
+        $query = "INSERT INTO `respuesta` (`Respuesta_texto`, `Correcta`, `Pregunta_ID`) VALUES ('$respuesta1', '1', '$idPregunta')";
         $this->database->insert($query);
         
-        
-        $query = "INSERT INTO `respuestas` (`Respuesta_texto`, `Correcta`, `Tematica_ID`) VALUES ('$respuesta2', '0', '$tematicaId')";
+        $query = "INSERT INTO `respuesta` (`Respuesta_texto`, `Correcta`, `Pregunta_ID`) VALUES ('$respuesta2', '0', '$idPregunta')";
         $this->database->insert($query);
         
-        $query = "INSERT INTO `respuestas` (`Respuesta_texto`, `Correcta`, `Tematica_ID`) VALUES ('$respuesta3', '0', '$tematicaId')";
+        $query = "INSERT INTO `respuesta` (`Respuesta_texto`, `Correcta`, `Pregunta_ID`) VALUES ('$respuesta3', '0', '$idPregunta')";
         $this->database->insert($query);
         
-        $query = "INSERT INTO `respuestas` (`Respuesta_texto`, `Correcta`, `Tematica_ID`) VALUES ('$respuesta4', '0', '$tematicaId')";
+        $query = "INSERT INTO `respuesta` (`Respuesta_texto`, `Correcta`, `Pregunta_ID`) VALUES ('$respuesta4', '0', '$idPregunta')";
         $this->database->insert($query);
 
         $query = "DELETE FROM `preguntas_reportadas` WHERE `id_pregunta_reportada` = '$idPregunta'";
